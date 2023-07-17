18a062d8d3d5f20dee9d2646eac28852638adb8d
Robert Watt <wattrg@hotmail.com>
2023-07-17 10:29:02 +1000

    Add new example for a temperature gradient in the boltzmann equation

---
+++ examples/boltzmann/temp_gradient/run.sh
@@ -0,0 +1,4 @@
+beq clean
+beq prep temp_gradient.py
+beq run
+beq post animate_macroscopic density
---
+++ examples/boltzmann/temp_gradient/temp_gradient.py
@@ -0,0 +1,32 @@
+config.title = "boltzmann quiescent flow"
+
+v = 10
+L = 10
+flow_time = 1 # L / v
+n = 3
+
+n_vel_increments = 300
+
+def initial_condition(x):
+    T = 273 + 1.0 * x    
+    return FlowState(rho = 1.3, T = T, v = v)
+
+config.domain = Domain()
+config.domain.number_cells = n
+config.domain.length = L
+
+config.gas_model = GasModel("air")
+
+config.equation = Boltzmann()
+config.equation.min_v = -4000
+config.equation.max_v = 4000
+config.equation.n_vel_increments = n_vel_increments
+
+config.solver = RungeKutta()
+config.solver.max_step = 1000000
+config.solver.max_time = flow_time
+config.solver.print_frequency = 100
+config.solver.plot_frequency = flow_time / 20
+config.solver.cfl = 1.0
+
+config.initial_condition = initial_condition
