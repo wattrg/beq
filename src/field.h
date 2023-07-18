32efafe26b41eb5f7c77935e1412230219a239e9
Robert Watt <wattrg@hotmail.com>
2023-07-18 15:14:02 +1000

    Field class now allows ghost cells

--- src/field.h
+++ src/field.h
@@ -9,11 +9,11 @@ template <class T>
 struct Field {
     Field () {}
 
-    Field (unsigned length, unsigned components, bool gpu)
-        : _length(length), _components(components), _gpu(gpu)
+    Field (unsigned length, unsigned nghost, unsigned components, bool gpu)
+        : _length(length), _nghost(nghost), _components(components), _gpu(gpu)
     {
         if (_gpu) {
-            auto code = cudaMalloc(&_data, _length * _components * sizeof(T)); 
+            auto code = cudaMalloc(&_data, (_length+2*_nghost) * _components * sizeof(T)); 
 
             if (code != cudaSuccess) {
                 std::cerr << "cudaMalloc failure: " << cudaGetErrorString(code) << std::endl;
@@ -21,11 +21,13 @@ struct Field {
             }
         } 
         else {
-            _data = new T[_length*_components];
+            _data = new T[(_length+2*_nghost)*_components];
         }
     }
 
-    Field (unsigned length, unsigned components) : Field(length, components, true) {}
+    Field (unsigned length, unsigned components, bool gpu) : Field(length, 0, components, gpu) {}
+    Field (unsigned length, unsigned nghost, unsigned components) : Field(length, nghost, components, true) {}
+    Field (unsigned length, unsigned components) : Field(length, 0, components, true) {}
     Field (unsigned length, bool gpu) : Field(length, 1, gpu) {}
     Field (unsigned length) : Field(length, 1, true) {}
 
@@ -40,12 +42,12 @@ struct Field {
 
     T& operator() (unsigned i) {
         assert(i < _length && _components == 1);
-        return _data[i];
+        return _data[i + _nghost];
     }
 
     T& operator() (unsigned i, unsigned comp) {
         assert(i < _length && comp < _components);
-        return _data[_length * comp + i];
+        return _data[(_length+2*_nghost) * comp + i + _nghost];
     }
 
     T& flat_index(unsigned i) {
@@ -54,13 +56,14 @@ struct Field {
 
     unsigned length () const { return _length; }
     unsigned number_components () const { return _components; }
-    unsigned size() const { return _components * _length; }
-    std::size_t memory() const { return _components * _length * sizeof(T); }
+    unsigned size() const { return _components * (_length+2*_nghost); }
+    std::size_t memory() const { return _components * (_length+2*_nghost) * sizeof(T); }
     T* data() { return _data; }
 
 private:
     T * _data = nullptr;
     unsigned _length;
+    unsigned _nghost;
     unsigned _components;
     bool _gpu;
 };
